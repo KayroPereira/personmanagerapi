b0c200a869cd1221d0a039b174cb15ab031977f4
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-07-21 12:03:06 -0300

    Adicionado dependência H2 Implementado funções de persistência JPA

--- src/main/java/br/com/kayropereira/personmanager/personmanagerapi/controller/PersonController.java
+++ src/main/java/br/com/kayropereira/personmanager/personmanagerapi/controller/PersonController.java
@@ -1,15 +1,30 @@
 package br.com.kayropereira.personmanager.personmanagerapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.kayropereira.personmanager.personmanagerapi.entity.Person;
+import br.com.kayropereira.personmanager.personmanagerapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
+    private PersonRepository personRepository;
+
+    //Injeção de dependência - Classe PersonRepository
+    /*
+        Fazer a injeção no construtor facilita no futuro a implementação
+        de testes unitários utilizando moke
+     */
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    //@RequestBoby informa que o objeto está vindo de uma requisição
+    @PostMapping
+    public String createPerson(@RequestBody Person person){
+        personRepository.save(person);
         return "API Test!";
     }
 }
