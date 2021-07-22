15ac5a6a90833c4bf1683fe91d787712c6b23bfe
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-07-22 10:59:01 -0300

    Implementado as consultas de pessoas

--- src/main/java/br/com/kayropereira/personmanager/personmanagerapi/controller/PersonController.java
+++ src/main/java/br/com/kayropereira/personmanager/personmanagerapi/controller/PersonController.java
@@ -2,12 +2,14 @@ package br.com.kayropereira.personmanager.personmanagerapi.controller;
 
 import br.com.kayropereira.personmanager.personmanagerapi.dto.request.PersonDTO;
 import br.com.kayropereira.personmanager.personmanagerapi.dto.response.MessageResponseDTO;
+import br.com.kayropereira.personmanager.personmanagerapi.exceptioin.PersonNotFoundException;
 import br.com.kayropereira.personmanager.personmanagerapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -24,6 +26,16 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED) //Retornar o código 201 em caso de sucesso
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 
     //    Versão de chamada dos métodos dentro do controller - não é uma boa prática
