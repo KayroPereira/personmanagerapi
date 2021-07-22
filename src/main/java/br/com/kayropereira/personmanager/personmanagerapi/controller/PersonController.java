23a4f299a345a23f59f6bd4b366d881a53766126
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-07-21 21:11:36 -0300

    Finalizadas as Funções de POST person

--- src/main/java/br/com/kayropereira/personmanager/personmanagerapi/controller/PersonController.java
+++ src/main/java/br/com/kayropereira/personmanager/personmanagerapi/controller/PersonController.java
@@ -1,11 +1,13 @@
 package br.com.kayropereira.personmanager.personmanagerapi.controller;
 
+import br.com.kayropereira.personmanager.personmanagerapi.dto.request.PersonDTO;
 import br.com.kayropereira.personmanager.personmanagerapi.dto.response.MessageResponseDTO;
-import br.com.kayropereira.personmanager.personmanagerapi.entity.Person;
 import br.com.kayropereira.personmanager.personmanagerapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -20,8 +22,8 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) //Retornar o código 201 em caso de sucesso
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
     }
 
     //    Versão de chamada dos métodos dentro do controller - não é uma boa prática
